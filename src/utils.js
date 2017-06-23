234da6346b2c2324d5ab1a1f324dfa3b3ff33a30
zppro <zpprozppro@gmail.com>
2017-06-23 16:39:33 +0800

    utils && dictBuilder func test ok

--- src/utils.js
+++ src/utils.js
@@ -2,21 +2,94 @@
  * Created by zppro on 17-6-22.
  */
 
-// const crypto = require('crypto');
-// const binding = process.binding('crypto');
-// const randomBytes = process.binding('crypto').randomBytes;
-const fs = require('fs-extra');
-const path = require('path');
-const through2 = require('through2');
+import klaw from 'klaw';
+import path from 'path';
+import through2 from 'through2';
+import moment from 'moment';
 
-
-export const isString =  (o) => {
+export const isString = (o) => {
     return Object.prototype.toString.call(o) == '[object String]';
 };
 
 export const isObject = (o) => {
     return o === Object(o);
 };
+
+export const pick = (o, ...keys) => {
+    let result = {};
+    if (o) {
+        if(Array.isArray(o)){
+            return o.map(o => pick.apply(null, [o].concat(keys)));
+        } else if(isObject(o)) {
+            for (let k of keys) {
+                if (o[k]) {
+                    result[k] = o[k];
+                }
+            }
+        }
+    }
+    return result;
+}
+
+export const pluck = (arr, key) => {
+    return arr.map(o => o[key])
+};
+
+export const values = (o) => {
+    return Object.keys(o).map(k => o[k]);
+};
+
+
+export const range = (start, stop, step) => {
+    if (stop == null) {
+        stop = start || 0;
+        start = 0;
+    }
+    step = step || 1;
+
+    let length = Math.max(Math.ceil((stop - start) / step), 0);
+    let current = start;
+    return Array.from({length: length}, (v, k) => {
+        k === 0 ? current : current += step;
+        return current;
+    });
+};
+
+const _rangeDate = (start, end, format, type) => {
+    let s = moment(start), e = end ? moment(end) : moment(), _f;
+    !type && (type = 'months');
+    switch (type) {
+        case 'months':
+            _f = 'YYYY-MM';
+            break;
+        case 'years':
+            _f = 'YYYY';
+            break;
+        case 'days':
+            _f = 'YYYY-MM-DD';
+            break;
+        default:
+            return [];
+            break;
+    }
+
+    let f = format || _f, ret = [];
+    for (let d = s; e.diff(d) >= 0; d = d.add(1, type)) {
+        ret.push(d.format(f));
+    }
+    return ret;
+}
+
+export const rangeDateAsMonth = (start, end, format) => {
+    return _rangeDate(start, end, format);
+};
+export const rangeDateAsYear = (start, end, format) => {
+    return _rangeDate(start, end, format, 'years');
+};
+export const rangeDateAsDay = (start, end, format) => {
+    return _rangeDate(start, end, format, 'days');
+};
+
 
 export const setProperty = (o,k,v) => {
     if (isObject(o)) {
@@ -27,12 +100,12 @@ export const setProperty = (o,k,v) => {
 };
 
 export const setPropertyRecursion = (o,k,v) => {
-    if(isObject(o)){
-        if(o.hasOwnProperty(k)){
+    if(isObject(o)) {
+        if (o.hasOwnProperty(k)) {
             o[k] = v;
         }
         else {
-            for (let oc in o) {
+            for (let oc of Object.keys(o)) {
                 setPropertyRecursion(o[oc], k, v);
             }
         }
@@ -59,11 +132,9 @@ export const setPropertyDotExpression = (o,k,v) => {
 };
 
 export const getPropertyCount = (o) => {
-    let n, count = 0;
-    for(n in o){
-        if(o.hasOwnProperty(n)){
-            count++;
-        }
+    let count = 0;
+    for (let k of Object.keys(o)) {
+        count++;
     }
     return count;
 }
@@ -77,14 +148,25 @@ export const randomS = (len) => {
     return process.binding('crypto').randomBytes(len).toString('hex').substr(0, len);
 };
 
-export const readDirectoryStructure = (dir, exts, options) => {
+export const readDirectoryStructure = (dir, options) => {
     dir = path.normalize(dir);
     if (dir.endsWith('/')) {
         dir = dir.substr(0, dir.length - 1);
     }
     options = options || {};
+    let exts = options.exts;
+    let excludeDirs = options.excludeDirs;
+    if (excludeDirs) {
+        if(!Array.isArray(excludeDirs)){
+            excludeDirs = [excludeDirs];
+        }
+        excludeDirs = excludeDirs.map((o)=>{return path.join(dir, o);})
+    }
+    let testExclude = (excludes, testPath) => {
+        return excludes.some((o)=>{ return testPath.startsWith(o);});
+    }
     let filter = through2.obj(function (item, enc, next) {
-        if (dir != item.path) {
+        if (dir != item.path && !(excludeDirs && testExclude(excludeDirs, item.path))) {
             if (!exts) {
                 this.push(item);
             }
@@ -108,16 +190,14 @@ const _transformTree = (dir, filter) => {
     let promise  = _transformObject(dir,filter).then(function(ret){
         let fileKeys = ['name', 'relative_name', 'path', 'relative_path'];
         let treeNodes = [], dirNode, children;
-        for(let key in ret) {
-            if (ret.hasOwnProperty(key)) {
-                if (!fileKeys.includes(key) && isObject(ret[key])) {
-                    dirNode = {_id: ret[key].relative_path || key, name: key};
-                    children = _parseChildren(ret[key], fileKeys);
-                    if(children && children.length > 0) {
-                        dirNode.children = children;
-                    }
-                    treeNodes.push(dirNode)
+        for(let key of Object.keys(ret)) {
+            if (!fileKeys.includes(key) && isObject(ret[key])) {
+                dirNode = {_id: ret[key].relative_path || key, name: key};
+                children = _parseChildren(ret[key], fileKeys);
+                if (children && children.length > 0) {
+                    dirNode.children = children;
                 }
+                treeNodes.push(dirNode)
             }
         }
         return treeNodes;
@@ -127,16 +207,14 @@ const _transformTree = (dir, filter) => {
 
 const _parseChildren = (obj, fileKeys) => {
     let treeNodes = [], dirNode, children;
-    for(let key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            if (!fileKeys.includes(fileKeys, key) && isObject(obj[key])) {
-                dirNode = {_id: obj[key].relative_path || key, name: key};
-                children = _parseChildren(obj[key], fileKeys);
-                if (children && children.length > 0) {
-                    dirNode.children = children;
-                }
-                treeNodes.push(dirNode)
+    for(let key of Object.keys(obj)) {
+        if (!fileKeys.includes(fileKeys, key) && isObject(obj[key])) {
+            dirNode = {_id: obj[key].relative_path || key, name: key};
+            children = _parseChildren(obj[key], fileKeys);
+            if (children && children.length > 0) {
+                dirNode.children = children;
             }
+            treeNodes.push(dirNode)
         }
     }
     return treeNodes;
@@ -145,7 +223,7 @@ const _parseChildren = (obj, fileKeys) => {
 const _transformObject = (dir, filter) => {
     let promise = new Promise(resolve=> {
             let ret = {};
-            fs.walk(dir)
+            klaw(dir)
                 .pipe(filter)
                 .on('data', function (item) {
                     let baseName = path.basename(item.path);
@@ -181,45 +259,34 @@ const _transformObject = (dir, filter) => {
     return promise;
 };
 
-export const chunkArrayRange = (arr, range) => {
+export const chunkArrayByCapacity = (arr, capacity) => {
     let result = [], start, end;
     // console.log('total arr length:', arr.length);
-    for (let x = 0; x < arr.length; x = x + range) {
-        start = result.length * range;
-        end = (start + range) < arr.length ? (start + range) : arr.length + 1;
+    for (let x = 0; x < arr.length; x = x + capacity) {
+        start = result.length * capacity;
+        end = (start + capacity) < arr.length ? (start + capacity) : arr.length + 1;
         // console.log('chunkArrayRange:', start, end);
         result.push(arr.slice(start, end));
     }
     return result;
 };
 
-export const chunkArraySize = (arr, size) => {
+export const chunkArrayByQuantity = (arr, quantity) => {
     let result = [], start, end;
-    for (let x = 0; x < Math.ceil(arr.length / size); x++) {
-        start = x * size;
-        end = start + size;
+    let remainder = arr.length % quantity;
+    let step = Math.ceil(arr.length / quantity);
+    let max = remainder > 0 ? quantity - 1: quantity;
+    for (let x = 0; x < max; x++) {
+        start = x * step;
+        end = start + step;
         result.push(arr.slice(start, end));
+    }
+    if(remainder > 0) {
+        result.push(arr.slice((quantity - 1) * step, quantity * step));
     }
     return result;
 };
 
-export const unflatten = (o) => {
-    if (!isObject(o) || Array.isArray(o))
-        return o;
-    let regex = /\.?([^.\[\]]+)|\[(\d+)\]/g,
-        resultholder = {};
-    for (let p in o) {
-        let cur = resultholder,
-            prop = "",
-            m;
-        while (m = regex.exec(p)) {
-            cur = cur[prop] || (cur[prop] = (m[2] ? [] : {}));
-            prop = m[2] || m[1];
-        }
-        cur[prop] = o[p];
-    }
-    return resultholder[""] || resultholder;
-};
 
 const recurse = (result, cur, prop) => {
     if (Object(cur) !== cur) {
@@ -231,7 +298,7 @@ const recurse = (result, cur, prop) => {
             result[prop] = [];
     } else {
         var isEmpty = true;
-        for (let p in cur) {
+        for (let p of Object.keys(cur)) {
             isEmpty = false;
             recurse(result, cur[p], prop ? prop+"."+p : p);
         }
@@ -244,6 +311,24 @@ export const flatten = (data) => {
     let result = {};
     recurse(result, data, "");
     return result;
+};
+
+export const unflatten = (o) => {
+    if (!isObject(o) || Array.isArray(o))
+        return o;
+    let regex = /\.?([^.\[\]]+)|\[(\d+)\]/g,
+        resultholder = {};
+    for (let p of Object.keys(o)) {
+        let cur = resultholder,
+            prop = "",
+            m;
+        while (m = regex.exec(p)) {
+            cur = cur[prop] || (cur[prop] = (m[2] ? [] : {}));
+            prop = m[2] || m[1];
+        }
+        cur[prop] = o[p];
+    }
+    return resultholder[""] || resultholder;
 };
 
 
---
+++ test/dictBuilder.test.js
@@ -0,0 +1,99 @@
+/**
+ * Created by zppro on 17-6-23.
+ */
+import { expect } from 'chai';
+import { join } from 'path'
+import dictBuilder from '../src/dictBuilder'
+
+describe('module [dictBuilder]', () => {
+    let d1 = dictBuilder(), slot1='slot1', slot2='slot2', slot3='slot3',
+    slot4='slot4', slot5='slot5', slot6='slot6', slot7='slot7', slot8='slot8';
+
+    it( `test dict add value = "v1,v2" `, () => {
+        d1.add(slot1,`v1,v2`);
+        expect(d1.keys[slot1][0]).to.be.equal(d1.vals[slot1][0]);
+        expect(d1.vals[slot1][0]).to.be.equal('v1');
+        expect(d1.keys[slot1][1]).to.be.equal(d1.vals[slot1][1]);
+        expect(d1.vals[slot1][1]).to.be.equal('v2');
+    });
+
+    it( `test dict add value = "k1=v1,k2=v2" `, () => {
+        d1.add(slot2,`k1=v1,k2=v2`);
+        expect(d1.keys[slot2][0]).to.be.equal('k1');
+        expect(d1.vals[slot2][0]).to.be.equal('v1');
+        expect(d1.keys[slot2][1]).to.be.equal('k2');
+        expect(d1.vals[slot2][1]).to.be.equal('v2');
+    });
+
+    it( `test dict add value = "k1:v1,k2:v2" `, () => {
+        d1.add(slot3,`k1:v1,k2:v2`);
+        expect(d1.keys[slot3][0]).to.be.equal('k1');
+        expect(d1.vals[slot3][0]).to.be.equal('v1');
+        expect(d1.keys[slot3][1]).to.be.equal('k2');
+        expect(d1.vals[slot3][1]).to.be.equal('v2');
+    });
+
+    it( `test dict add value = {"k1":"v1","k2":"v2"} `, () => {
+        d1.add(slot4, {"k1":"v1","k2":"v2"});
+        expect(d1.keys[slot4][0]).to.be.equal('k1');
+        expect(d1.vals[slot4][0]).to.be.equal('v1');
+        expect(d1.keys[slot4][1]).to.be.equal('k2');
+        expect(d1.vals[slot4][1]).to.be.equal('v2');
+    });
+
+    it( `test dict add value = ["v1","v2"] `, () => {
+        d1.add(slot5, ["v1","v2"]);
+        expect(d1.keys[slot5][0]).to.be.equal(d1.vals[slot5][0]);
+        expect(d1.vals[slot5][0]).to.be.equal('v1');
+        expect(d1.keys[slot5][1]).to.be.equal(d1.vals[slot5][1]);
+        expect(d1.vals[slot5][1]).to.be.equal('v2');
+    });
+
+    it( `test dict add value = [{k:"k1",v:"v1"},{k:"k2",v:"v2"}] `, () => {
+        d1.add(slot6, [{k:"k1",v:"v1"},{k:"k2",v:"v2"}]);
+        expect(d1.keys[slot6][0]).to.be.equal('k1');
+        expect(d1.vals[slot6][0]).to.be.equal('v1');
+        expect(d1.keys[slot6][1]).to.be.equal('k2');
+        expect(d1.vals[slot6][1]).to.be.equal('v2');
+    });
+
+    it( `test dict add value = [["k1","v1"],["k2","v2"]] `, () => {
+        d1.add(slot7, [["k1","v1"],["k2","v2"]]);
+        expect(d1.keys[slot7][0]).to.be.equal('k1');
+        expect(d1.vals[slot7][0]).to.be.equal('v1');
+        expect(d1.keys[slot7][1]).to.be.equal('k2');
+        expect(d1.vals[slot7][1]).to.be.equal('v2');
+    });
+
+    it( `test dict add value = [{k:"k1",v:{"name":"姓名1","order":1}},{k:"k2",v:{"name":"姓名2","order":2}}] `, () => {
+        d1.add(slot8, [{k:"k1",v:{"name":"姓名1","order":1}},{k:"k2",v:{"name":"姓名2","order":2}}]);
+        expect(d1.keys[slot8][0]).to.be.equal('k1');
+        expect(d1.vals[slot8][0].name).to.be.equal('姓名1');
+        expect(d1.vals[slot8][0].order).to.be.equal(1);
+        expect(d1.keys[slot8][1]).to.be.equal('k2');
+        expect(d1.vals[slot8][1].name).to.be.equal('姓名2');
+        expect(d1.vals[slot8][1].order).to.be.equal(2);
+    });
+
+    it( `test dict readJson dictionary.json `, (done) => {
+        d1.clear();
+        let file = join(__dirname, 'dictionary.json');
+        d1.readJson(file).then(()=>{
+            expect(d1.pairs['IDC00']['name']).to.be.equal('产品类型(带票)');
+            expect(d1.pairs['IDC00']['A'].name).to.be.equal('景点');
+            expect(d1.pairs['IDC00']['A'].order).to.be.equal(1);
+            expect(d1.pairs['IDC00']['B'].name).to.be.equal('线路');
+            expect(d1.pairs['IDC00']['B'].order).to.be.equal(2);
+            expect(d1.pairs['IDC00']['C'].name).to.be.equal('酒店');
+            expect(d1.pairs['IDC00']['C'].order).to.be.equal(3);
+            expect(d1.pairs['IDC00']['F'].name).to.be.equal('套票');
+            expect(d1.pairs['IDC00']['F'].order).to.be.equal(6);
+            expect(d1.pairs['IDC00']['H'].name).to.be.equal('演出');
+            expect(d1.pairs['IDC00']['H'].order).to.be.equal(8);
+            expect(d1.pairs['IDC00']['I'].name).to.be.equal('未知');
+            expect(d1.pairs['IDC00']['I'].order).to.be.equal(10);
+            done();
+        });
+
+    });
+});
