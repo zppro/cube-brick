bcd83c77d399d4c1f60f67e9e88a150090e628d2
zppro <zpprozppro@gmail.com>
2017-08-28 16:51:53 +0800

    add customer header

--- src/lib/koa-middlewares/CORS.js
+++ src/lib/koa-middlewares/CORS.js
@@ -2,20 +2,25 @@
  * Created by zppro on 17-8-1.
  */
 
-export default (whitelist, { ignorePaths, logger }) => {
+export default (whitelist, { headers, methods, ignorePaths, logger }) => {
   const re = /http(s)?:\/\/192\.168\.([0-9]|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.([0-9]|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]):?[0-9]*/gi;//内网地址
+  const defaultHeaders = ['Content-Type', 'Content-Length', 'Authorization', 'Accept', 'X-Requested-With', 'Origin', 'Access-Control-Allow-Origin', 'X-Custom-TS']
+  const defaultMethods = ['PUT', 'POST', 'GET', 'DELETE', 'OPTIONS']
   return async (ctx, next) => {
     const {url, method} = ctx
     const isIgnore = ignorePaths && ignorePaths.find(o => url.startsWith(o))
+    console.log('isIgnore:', isIgnore)
     if (!isIgnore) {
       const origin = (ctx.request.headers['origin'] || '').toLowerCase();
       let patched = whitelist.includes(origin) || origin.match(re);
       if (patched) {
         logger && logger.d(`patched CORS for path ${method}: ${url}`)
+        console.log('allow Headers:', [...defaultHeaders, ...headers].join())
+        console.log('allow Methods:', (methods || defaultMethods).join())
         ctx.set('Access-Control-Allow-Origin', origin);
         ctx.set('Access-Control-Allow-Credentials', 'true');
-        ctx.set('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Authorization,Accept,X-Requested-With,Origin,Access-Control-Allow-Origin,X-Custom-TS');
-        ctx.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+        ctx.set('Access-Control-Allow-Headers', [...defaultHeaders, ...headers].join());
+        ctx.set('Access-Control-Allow-Methods', (methods || defaultMethods).join());
       }
       else {
         logger && logger.d(`not allow CORS for path ${method}: ${url}`)
@@ -26,6 +31,10 @@ export default (whitelist, { ignorePaths, logger }) => {
       }
     } else {
       logger && logger.d(`ignore CORS for path ${url}`)
+      ctx.set('Access-Control-Allow-Origin', origin);
+      ctx.set('Access-Control-Allow-Credentials', 'true');
+      ctx.set('Access-Control-Allow-Headers', [...defaultHeaders, ...headers].join());
+      ctx.set('Access-Control-Allow-Methods', (methods || defaultMethods).join());
     }
     await next()
   }
